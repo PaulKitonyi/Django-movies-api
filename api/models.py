69e3f168010dfef3b781984aa5e1ca1c79de0891
Paul Kitonyi <paulmucimah@gmail.com>
2018-08-16 23:31:42 +0300

    added models.py file

--- api/models.py
+++ api/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    """This class create the data representation of the app"""
+    name = models.Charfield(max_length=100)
+    year_of_release = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        """This method converts name object to a human readable string"""
