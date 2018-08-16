6805ce45afbe3d33a1747f0fdbd332bcf8be5b9d
Paul Kitonyi <paulmucimah@gmail.com>
2018-08-17 00:09:59 +0300

    add models.py

--- api/models.py
+++ api/models.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
     """This class create the data representation of the app"""
-    name = models.Charfield(max_length=100)
+    name = models.CharField(max_length=100)
     year_of_release = models.PositiveSmallIntegerField()
 
     def __str__(self):
